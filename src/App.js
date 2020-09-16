9e807c51d15742782bb9871117a689c47307e092
Zam Hassan <zameerulhassan@gmail.com>
2020-09-16 09:54:06 -0600

    adds fontawesome and icon in navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from './components/layout/Navbar'
 class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <h1>hello</h1>
+      <Navbar title="Github Finder" icon="fab fa-github"/>
       </div>
-    );
+    ); 
   }
 }
 
