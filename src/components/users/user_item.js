6981ee3a6fd9ce56b9b1ad90d2c0c2f2d04efcb7
Zam Hassan <zameerulhassan@gmail.com>
2020-09-18 10:14:14 -0600

    adds state without constructor & super plus de-structuring

--- src/components/users/user_item.js
+++ src/components/users/user_item.js
@@ -2,22 +2,19 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    //console.log(123)
-    this.state = {
-      id: "id",
-      login: "mojobo",
-      avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-      html_url: "https://github.com/mojombo",
-    };
-  }
+    state = {
+        id: "id",
+        login: "mojobo",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        html_url: "https://github.com/mojombo",
+      };
   render(props) {
+      const {avatar_url,login,html_url}=this.state
     return <div className="card text-center">
-    <img src={this.state.avatar_url} alt="" className="round-img" style={{width:"100px"}} />
-    <h3>{this.state.login}</h3>
+    <img src={avatar_url} alt="" className="round-img" style={{width:"100px"}} />
+    <h3>{login}</h3>
     <div>
-    <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">more</a>
+    <a href={html_url} className="btn btn-dark btn-sm my-1">more</a>
     </div>
     </div>;
   }
