c4f7a9743829d297711b246a258a48542a9a424b
Zam Hassan <zameerulhassan@gmail.com>
2020-09-19 22:18:44 -0600

    changes class components to functions except app and Usrs.js adds: app level state inside app.js by using componentDidMount()

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import axios from 'axios'
 class App extends React.Component {
+  state={
+    users:[],
+    isLoading:false
+  }
+  // componentDidMount(){ //for now just getting app level state to share data with componenets
+  //   //console.log(123)
+  //   axios.get('https://api.github.com/users').then(res=>{
+  //     console.log(res.data)
+  //   }).catch(err=>{
+  //     console.log(err)
+  //   })
+
+  // }
+  async componentDidMount(){ //for now just getting app level state to share data with componenets
+    //console.log(123)
+    this.setState({isLoading:true})
+    const res= await axios.get('https://api.github.com/users');
+    //console.log(res.data)
+    this.setState({isLoading:false,users:res.data})
+  }
   render(){
     return (
       <div className="App">
       <Navbar />
       <div className="container">
-      <Users />
+      <Users isLoading={this.state.isLoading} users={this.state.users}/>
       </div>
       </div>
     ); 
--- src/components/users/user_item.js
+++ src/components/users/user_item.js
@@ -1,9 +1,9 @@
 // @ts-nocheck
 //stateless functional component
 import React from "react";
+import propTypes from 'prop-types'
 
-const UserItem =(props)=> {
-    const {avatar_url,login,html_url}=props.user
+const UserItem =({user:{avatar_url,login,html_url}})=> {
     return <div className="card text-center">
     <img src={avatar_url} alt="" className="round-img" style={{width:"60px"}} />
     <h3>{login}</h3>
@@ -11,5 +11,8 @@ const UserItem =(props)=> {
     <a href={html_url} className="btn btn-dark btn-sm my-1">more</a>
     </div>
     </div>;
+}
+UserItem.propTypes={
+    user: propTypes.object.isRequired
 }
 export default UserItem;
