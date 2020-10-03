20477f21a96fcb229fc6a919c2a81beb0308ba0b
Zam Hassan <zameerulhassan@gmail.com>
2020-10-03 16:37:00 -0600

    about> Courtesy to Inpiration

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,15 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 export const About = () => {
-    return (
-        <Fragment>
-            <h1>About This App</h1>
-            <p>App to search Github users</p>
-            <p>Version:1.0.0</p>
-            <p>Created: 24 September 2020</p>
-            <p>Courtesy: Brad Traversy</p>
-            <i>nymhsn13@gmail.com</i>
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>App to search Github users</p>
+      <p>Version:1.0.0</p>
+      <p>Created: 24 September 2020</p>
+      <p>Inpiration: Brad Traversy</p>
+      <i>nymhsn13@gmail.com</i>
+    </Fragment>
+  );
+};
 export default About;
